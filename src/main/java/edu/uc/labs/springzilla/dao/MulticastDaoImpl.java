0e495205498d13eeb666a32b053dd0fe253c63b1
Matt Silberngel <silbermm@gmail.com>
2013-03-25 10:13:33 -0400

    added functionality to MulticastDaoImpl

--- src/main/java/edu/uc/labs/springzilla/dao/MulticastDaoImpl.java
+++ src/main/java/edu/uc/labs/springzilla/dao/MulticastDaoImpl.java
@@ -31,17 +31,17 @@ public class MulticastDaoImpl implements MulticastDao {
             } else if (line.matches(ttlPattern.pattern())) {
                 Matcher matcher = ttlPattern.matcher(line);
                 while (matcher.find()) {
-                    //ms.setMulticastTTL(matcher.group(1));
+                    line = line.replaceAll(ttlPattern.pattern(), ttlReplacement + "\"--ttl " + settings.getMulticastTTL() + "\"");
                 }
             } else if (line.matches(rdvAddress.pattern())) {
                 Matcher matcher = rdvAddress.matcher(line);
                 while (matcher.find()) {
-                    //ms.setRdvAddress(matcher.group(1));
+                    line = line.replaceAll(rdvAddress.pattern(), rdvReplacement + "\"" + settings.getRdvAddress() + "\"");
                 }
             } else if (line.matches(senderAddress.pattern())) {
                 Matcher matcher = senderAddress.matcher(line);
                 while (matcher.find()) {
-                    //ms.setSenderAddress(matcher.group(1));
+                    line = line.replaceAll(senderAddress.pattern(), senderReplacement + "\"--multicast-data-address " + settings.getSenderAddress() + "\"");
                 }
             }
             newoutput += line + "\n";
@@ -104,6 +104,9 @@ public class MulticastDaoImpl implements MulticastDao {
     private Pattern portPattern = Pattern.compile("MULTICAST_PORT=\"(\\d*)\"");
     private String portReplacement = "MULTICAST_PORT=";
     private Pattern ttlPattern = Pattern.compile("TIME_TO_LIVE_OPT=\"--ttl (\\d*)\"");
+    private String ttlReplacement = "TIME_TO_LIVE_OPT=";
     private Pattern rdvAddress = Pattern.compile("MULTICAST_ALL_ADDR=\"([[\\d]+\\.[\\d]+\\.[\\d]+\\.[\\d]+]*)\"");
+    private String rdvReplacement = "MULTICAST_ALL_ADDR=";
     private Pattern senderAddress = Pattern.compile("udp_sender_extra_opt_default=\"--mcast-data-address ([\\d]+\\.[\\d]+\\.[\\d]+\\.[\\d]+)\"", Pattern.CASE_INSENSITIVE);
+    private String senderReplacement = "udp_sender_extra_opt_default=";
 }
